1a3687fd86d4164e878af4b337c8722d032da22e
Lakshya Bansal <39180629+lakshya-20@users.noreply.github.com>
2020-10-19 19:59:47 +0530

    Added Carousel for recommendations

--- src/components/screens/HomePage.js
+++ src/components/screens/HomePage.js
@@ -3,6 +3,9 @@ import {usercontext} from'../../App'
 import {backendURL,flaskBackendURL} from '../../Config'
 import { Card, CardImg, CardImgOverlay,CardTitle,CardBody,CardText, 
     Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+
 import {Link} from 'react-router-dom';
 import { Loading } from './Loading'
 
@@ -16,15 +19,35 @@ function RenderMovie ({movie}) {
                 </CardImgOverlay> */}
                 <CardBody className="text-dark">
                     <CardTitle className="font-weight-bold">{movie.title}</CardTitle>
-                    <CardText>
+                    {/* <CardText>
                         {movie.genres}
-                    </CardText>
+                    </CardText> */}
                     
                 </CardBody>
             </Link>
         </Card>
     );
 }
+
+const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 7
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 5
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 3
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 2
+    }
+};
 
 const Homepage=()=>{
     const[recomd_movies_data,setRecomdMoviesData]=useState([])
@@ -60,15 +83,19 @@ const Homepage=()=>{
                 {
                     recomd_movies_data.length!=0?
                         <div className="row justify-content-center">
-                            {
-                                recomd_movies_data.map((movie) => {
-                                    return (
-                                        <div className="col-5 col-md-3  py-3"  key={movie._id}>
-                                            <RenderMovie movie={movie}  />
-                                        </div>
-                                    );
-                                })
-                            }
+                            <div className="col-12">
+                                <Carousel responsive={responsive}>
+                                    {
+                                        recomd_movies_data.map((movie) => {
+                                            return (
+                                                <div className="col-12 border"  key={movie._id}>
+                                                    <RenderMovie movie={movie}  />
+                                                </div>
+                                            );
+                                        })
+                                    }
+                                </Carousel>
+                            </div>
                         </div>
                     :
                         <div className="container">
