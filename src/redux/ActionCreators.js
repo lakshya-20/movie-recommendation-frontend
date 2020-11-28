1868a9431b9d53fc99f5f27575575a2e0bf7b0d8
Lakshya Bansal <39180629+lakshya-20@users.noreply.github.com>
2020-11-28 15:45:01 +0530

    Added redux functions

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -1,5 +1,5 @@
 import * as ActionTypes from './ActionTypes';
-import {backendURL} from '../Config';
+import {backendURL,flaskBackendURL} from '../Config';
 
 
 //Movies Handling
@@ -7,8 +7,9 @@ export const moviesLoading=()=>({
     type:ActionTypes.MOVIES_LOADING
 })
 
-export const moviesFailed=()=>({
-    type:ActionTypes.MOVIES_FAILED
+export const moviesFailed=(errMess)=>({
+    type:ActionTypes.MOVIES_FAILED,
+    payload:errMess
 })
 
 export const addMovies=(movies)=>({
@@ -16,35 +17,117 @@ export const addMovies=(movies)=>({
     payload:movies
 })
 
-//Rating Handling
-export const userRatingLoading=()=>({
-    type:ActionTypes.USER_RATING_LOADING
+export const fetchMovies=()=>(dispatch)=>{
+    dispatch(moviesLoading());
+    return fetch(backendURL+'/api/movies',{
+        headers:{
+            "Authorization":"Bearer "+localStorage.getItem("jwt")
+        }
+    })
+    .then(response => {
+        if (response.ok) {
+            return response;
+        } 
+        else {
+            var error = new Error('Error ' + response.status + ': ' + response.statusText);
+            error.response = response;
+            throw error;
+        }
+    },
+    error => {
+        var errMess = new Error(error.message);
+        dispatch(moviesFailed(errMess));
+        throw errmess;
+    })
+    .then(response => response.json())
+    .then(movies => dispatch(addMovies(movies)))
+}
+
+//Reviews Handling
+export const userReviewsLoading=()=>({
+    type:ActionTypes.USER_REVIEWS_LOADING
 })
 
-export const userRatingFailed=()=>({
-    type:ActionTypes.USER_RATING_FAILED
+export const userReviewsFailed=(errMess)=>({
+    type:ActionTypes.USER_REVIEWS_FAILED,
+    payload:errMess
 })
 
-export const addUserRating=(userRating)=>({
-    type:ActionTypes.ADD_USER_RATING,
-    payload:userRating
+export const addUserReviews=(userReviews)=>({
+    type:ActionTypes.ADD_USER_REVIEWS,
+    payload:userReviews
 })
+
+export const fetchUserReviews=(userId)=>(dispatch)=>{
+    dispatch(userReviewsLoading());
+    return fetch(backendURL+`/api/reviews${userId}`,{
+        headers:{
+            "Authorization":"Bearer "+localStorage.getItem("jwt")
+        }
+    })
+    .then(response => {
+        if (response.ok) {
+            return response;
+        } 
+        else {
+            var error = new Error('Error ' + response.status + ': ' + response.statusText);
+            error.response = response;
+            throw error;
+        }
+    },
+    error => {
+        var errMess = new Error(error.message);
+        dispatch(userReviewsFailed(errMess))
+        throw errmess;
+    })
+    .then(response => response.json())
+    .then(reviews => dispatch(addUserReviews(reviews)))
+}
 
 
 //Recommendations Handling
-export const recommendationLoading=()=>({
+export const recommendationsLoading=()=>({
     type:ActionTypes.RECOMMENDATIONS_LOADING
 })
 
-export const moviesFailed=()=>({
+export const recommendationsFailed=()=>({
     type:ActionTypes.RECOMMENDATIONS_FAILED
 })
 
-export const addMovies=(recommendations)=>({
+export const addRecommendations=(recommendations)=>({
     type:ActionTypes.ADD_RECOMMENDATIONS,
     payload:recommendations
 })
 
+export const fetchRecommendations=(userId)=>(dispatch)=>{
+    dispatch(recommendationsLoading());
+    return fetch(flaskBackendURL+`/recommendation/${userId}`)
+    .then(response => {
+        if (response.ok) {
+            return response;
+        } 
+        else {
+            var error = new Error('Error ' + response.status + ': ' + response.statusText);
+            error.response = response;
+            throw error;
+        }
+    },
+    error => {
+        var errMess = new Error(error.message);
+        dispatch(recommendationsFailed(errMess))
+        throw errmess;
+    })
+    .then(response => response.json())
+    .then(recommd => {
+        const movies=recommd
+        for(var i=0;i<movies.length;i++){
+            const movie=movies[i]
+            const id=movie._id.$oid
+            movies[i]._id=id
+        }
+        dispatch(addRecommendations(movies))
+    })
+}
 
 //AuthState Handling
 export const authStateLoading=()=>({
