c803fc8de99dea376a5407d0a9d3a523723cd6e1
Lakshya Bansal <bansallakshya94128@gmail.com>
2021-03-30 01:36:46 +0530

    Dispatching user credentials using OAuth

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React,{useEffect,createContext, useReducer, useContext} from 'react';
-
 import {BrowserRouter, Route, Switch, useHistory, Link} from 'react-router-dom'
+import queryString from 'query-string';
+import M from 'materialize-css'
+
 import NavBar from './components/Navbar'
 import HomePage from './components/screens/HomePage';
 import Login from './components/screens/Login';
 import Movies from './components/screens/Movies';
 import MovieDetails from './components/screens/MovieDetails'
 import Contact from './components/screens/Contact'
-import About from './components/screens/About'
+import About from './components/screens/About';
+
 import './App.css';
 import {reducer,initialState} from './reducers/userReducer'
-
+import {backendURL} from './Config'
 
 //redux
 import {Provider} from 'react-redux';
@@ -24,14 +27,43 @@ const Routing=()=>{
   const {state,dispatch}=useContext(usercontext)
   useEffect(()=>{
     //localStorage.clear()
+
+    //Using OAuth 
+    const parsed  = queryString.parse(window.location.search);
+    if(parsed.token){
+      fetch(backendURL+"/api/auth/oauth/util",{
+        method:"post",
+        headers:{
+          "Authorization":parsed.token
+        }
+      }).then(res=>res.json())
+      .then(data=>{
+          if(data.error){
+            M.toast({html: data.error,classes:"#c62828 red darken-3"})
+          }
+          else{ 
+              localStorage.setItem("jwt",data.token)
+              localStorage.setItem("user",JSON.stringify(data.user))
+              dispatch({type:"USER",payload:data.user})
+              history.push('/')
+          }
+      }).catch(err=>{
+          console.log(err)
+      })
+    }
+    //Oauth code ends here
+
     const user= JSON.parse(localStorage.getItem("user"))
     if(user){
       dispatch({type:"USER",payload:user})
     }
     else{
-      if(!history.location.pathname.startsWith('/reset'))
-           history.push('/')
+      if(!(history.location.pathname.startsWith('/reset'))){
+        history.push('/')
+      }      
     }
+    
+
   },[])
   return(
     <Switch>
@@ -50,6 +82,7 @@ const Routing=()=>{
       <Route path="/aboutus">
         <About />
       </Route>
+      
     </Switch>
   );
 }
