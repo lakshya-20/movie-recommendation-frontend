8901b8b28ae3f1b97d5eca2ea031fa8cd6ecee02
Lakshya Bansal <bansallakshya94128@gmail.com>
2021-04-11 00:22:08 +0530

    Changes in config file

--- src/Config.js
+++ src/Config.js
@@ -1,14 +1,16 @@
+var data={}
 if(process.env.NODE_ENV==='production'){
-    module.exports={
+    data={
         backendURL: process.env.BACKEND_URL,
         flaskBackendURL: process.env.FLASK_BACKEND_URL,
         JWT_SECRET: process.env.JWT_SEC
     }
 }
 else{
-    module.exports={
+    data={
         backendURL:"http://localhost:5000",
         flaskBackendURL:"http://localhost:5001",
         JWT_SECRET:"my_secret_key"
     }
 }
+module.exports=data;
