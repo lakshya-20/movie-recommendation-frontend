e9b4bfeeb708a513b989134ec2d829688f036b68
Lakshya Bansal <bansallakshya94128@gmail.com>
2021-04-11 16:54:39 +0530

    Changes in config file

--- src/Config.js
+++ src/Config.js
@@ -1,16 +1,20 @@
-var data={}
+var data={
+    backendURL:"http://localhost:5000",
+    flaskBackendURL:"http://localhost:5001",
+    JWT_SECRET:"my_secret_key"
+}
 if(process.env.NODE_ENV==='production'){
     data={
-        backendURL: process.env.BACKEND_URL,
-        flaskBackendURL: process.env.FLASK_BACKEND_URL,
+        backendURL: "https://flick--backend.herokuapp.com",
+        flaskBackendURL: "https://flick-flask.herokuapp.com",
         JWT_SECRET: process.env.JWT_SEC
     }
 }
-else{
-    data={
-        backendURL:"http://localhost:5000",
-        flaskBackendURL:"http://localhost:5001",
-        JWT_SECRET:"my_secret_key"
-    }
-}
+// else{
+//     data={
+//         backendURL:"http://localhost:5000",
+//         flaskBackendURL:"http://localhost:5001",
+//         JWT_SECRET:"my_secret_key"
+//     }
+// }
 module.exports=data;
