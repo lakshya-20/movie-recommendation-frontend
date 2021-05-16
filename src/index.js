5ff3066d122f6db1fa0742a2862e380cb316e7cf
Lakshya Bansal <bansallakshya94128@gmail.com>
2021-05-16 12:34:20 +0530

    Added star input and toast

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.css';
 import 'bootstrap-social/bootstrap-social.css';
+import 'react-toastify/dist/ReactToastify.css';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
+import {ToastContainer} from 'react-toastify';
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <ToastContainer position="bottom-center" pauseOnFocusLoss={false} autoClose={2000}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
