016d30a3636a01d11fa4ce35b6daf9e740bf8305
Lakshya Bansal <bansallakshya94128@gmail.com>
2021-11-19 18:37:59 +0530

    Added search by title feature(debouncing)

--- src/components/screens/MoviesPagination.js
+++ src/components/screens/MoviesPagination.js
@@ -3,7 +3,8 @@ import {Link} from 'react-router-dom';
 import ReactPaginate from 'react-paginate';
 import './Styles/Pagination.css'
 import MovieCard from './MovieCard';
-import styles from './Styles/MoviePagination.module.css'
+import styles from './Styles/MoviePagination.module.css';
+import {backendURL,flaskBackendURL} from '../../Config';
 function MoviesPagination(props) {
     const[movies_data,setMoviesData]=useState(props.movies_data)
     const [pagination, setPagination] = useState({
@@ -13,7 +14,33 @@ function MoviesPagination(props) {
         pageCount: 0,
         currentData: []
     });
+    const [searchTitle, setSearchTitle] = useState("");
+    const [searchMovieData, setSearchMovieData] = useState([]);
     
+    //Debouncing
+    function debounce(func, timeout = 300){
+        let timer;
+        return (...args) => {
+            clearTimeout(timer);
+            timer = setTimeout(() => func.apply(this, args), timeout);
+        }
+    }
+    function fetchData(title){
+        setSearchTitle(title)
+        if(title=="") {
+            setSearchTitle(undefined);
+            return;
+        }
+        fetch(backendURL+`/api/movies/find/${title}`,{
+            headers:{
+                "Authorization":localStorage.getItem("jwt")
+            }
+        }).then(res=>res.json())
+        .then(result=>{
+            setSearchMovieData(result)            
+        })
+    }
+    const processChange = debounce(fetchData,1000);
 
     useEffect(() => {
         setPagination((prevState) => ({
@@ -30,30 +57,28 @@ function MoviesPagination(props) {
         setPagination({ ...pagination, offset })
     }
 
-    return (
-        <div className="container">
-            <div className="row">
-                <nav aria-label="breadcrumb">
-                    <ol class="breadcrumb cust_breadcrumb">
-                        <li class="breadcrumb-item "><Link to='/' className="cust_breadcrumb_link">Home</Link></li>
-                        <li class="breadcrumb-item text-light active" aria-current="page">Movies</li>
-                    </ol>
-                </nav>
-                <div className="col-12">
-                    <h3 className="main_heading">Movies</h3>
-                    <hr />
-                </div> 
-            </div>
-            <div className="row justify-content-center">
-                {pagination.currentData && pagination.currentData.map(((item, index) => (
-                    <div className="col-6 col-md-3  py-3"  key={item._id}>
-                        {/* <RenderMovie movie={item}  /> */}
-                        <MovieCard movie={item}/>
-                    </div>
-                )))
-                }
-            </div>
-            
+    const renderAllMovies = () => {
+        return (
+            pagination.currentData && pagination.currentData.map(((item, index) => (
+                <div className="col-6 col-md-3  py-3"  key={item._id}>
+                    {/* <RenderMovie movie={item}  /> */}
+                    <MovieCard movie={item}/>
+                </div>
+            )))
+        )
+    }
+    const renderSearchMovies = () => {
+        return (
+            searchMovieData.map(((item, index) => (
+                <div className="col-6 col-md-3  py-3"  key={item._id}>
+                    {/* <RenderMovie movie={item}  /> */}
+                    <MovieCard movie={item}/>
+                </div>
+            )))
+        )
+    }
+    const renderPaginationControls = () => {
+        return (
             <div id="react-paginate" className={`row justify-content-center`}>
                 <ReactPaginate
                     previousLabel={'← Previous'}
@@ -73,7 +98,63 @@ function MoviesPagination(props) {
                     activeLinkClassName = {`${styles.activeLinkClassName}`}
                 />
             </div>
-        
+        )
+    }    
+    return (
+        <div className="container">
+            <div className="row">
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb cust_breadcrumb">
+                        <li class="breadcrumb-item "><Link to='/' className="cust_breadcrumb_link">Home</Link></li>
+                        <li class="breadcrumb-item text-light active" aria-current="page">Movies</li>
+                    </ol>
+                </nav>
+                <div className="col-12">
+                    <h3 className="main_heading col-12 col-sm-6 float-left">Movies</h3>
+                    {/* Search Movie */}
+                    <div className="search col-12 col-sm-6 float-right">
+                        <div className="d-flex justify-content-center">
+                            <input 
+                                type="text" 
+                                className="searchTerm" 
+                                placeholder="Search By Title"
+                                onChange={(e)=>processChange(e.target.value)}
+                            />
+                            <button type="submit" className="searchButton" onClick="">
+                                <i className="fa fa-search"></i>
+                            </button>
+                        </div>                
+                    </div>
+                    {/* Search Movie Ends */}                    
+                </div> 
+            </div>
+            <div className="row justify-content-center">
+                {searchTitle?    
+                    <>                                           
+                    {searchMovieData.length>0?
+                        <>
+                        <h5 className="col-12 d-flex justify-content-center">
+                            Showing Results for "{searchTitle}"
+                        </h5>                        
+                        {renderSearchMovies()}
+                        </>
+                    :
+                        <>                        
+                        <h5 className="col-12 d-flex justify-content-center">
+                            No movie found with title "{searchTitle}""
+                        </h5>
+                        {renderAllMovies()}    
+                        {renderPaginationControls()}                                            
+                        </>
+                    }
+                    </>
+                :
+                    <>
+                    {renderAllMovies()}
+                    {renderPaginationControls()}
+                    </>
+                }
+            </div>
         </div>
     );
 }
